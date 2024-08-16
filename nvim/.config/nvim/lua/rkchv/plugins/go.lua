13417020261903d604a03e66af3f7829fb77adc0
Roman Kochnev <rkochnevv@gmail.com>
2024-08-16 09:17:32 +0300

    add golang plugin

---
+++ nvim/.config/nvim/lua/rkchv/plugins/go.lua
@@ -0,0 +1,15 @@
+return {
+  "ray-x/go.nvim",
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()',
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+
+  config = function()
+    require("go").setup()
+  end,
+}
