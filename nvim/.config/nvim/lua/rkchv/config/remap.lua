f3389577941c5b32d84ba033c21dde59455adf6a
Roman Kochnev <rkochnevv@gmail.com>
2024-08-18 09:32:36 +0300

    remove unused

--- nvim/.config/nvim/lua/rkchv/config/remap.lua
+++ nvim/.config/nvim/lua/rkchv/config/remap.lua
@@ -91,10 +91,6 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- Formatting using LSP
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- Navigate throught quickfix list
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-
 -- Find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
@@ -111,3 +107,5 @@ end)
 -- vim.keymap.set("n", "n", "nzzzv")
 -- vim.keymap.set("n", "N", "Nzzzv")
 -- vim.keymap.set("x", "<leader>p", [["_dP]])
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
