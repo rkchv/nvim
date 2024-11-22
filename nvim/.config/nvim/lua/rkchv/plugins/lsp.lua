37e8531ce117fe05fb37c2bdee5eba9ab8fc5ddc
Roman Kochnev <rkochnevv@gmail.com>
2024-11-22 03:55:18 +0300

    bash lsp added

--- nvim/.config/nvim/lua/rkchv/plugins/lsp.lua
+++ nvim/.config/nvim/lua/rkchv/plugins/lsp.lua
@@ -1,11 +1,53 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    -- "jose-elias-alvarez/null-ls.nvim",
     enabled = true,
 
     config = function()
       local lspconfig = require('lspconfig')
+
+      lspconfig.bashls.setup({
+        cmd = { "bash-language-server", "start" },
+        filetypes = { "bash", "sh", "zsh" },
+        settings = {
+          bash = {
+            enable = true,
+            strict = true,
+            highlight = true,
+            completion = {
+              enable = true,
+              triggerAutoComplete = true,
+            },
+            hover = {
+              enable = true,
+            },
+            diagnostics = {
+              enable = true,
+              lint = true,
+            },
+            format = {
+              enable = true,
+              settings = {
+                tabWidth = 2,
+                indentStyle = "space",
+              }
+            }
+          }
+        }
+      })
+
+      lspconfig.yamlls.setup({
+        settings = {
+          yaml = {
+            format = {
+              enable = true,
+            },
+            schemas = {
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+            },
+          },
+        },
+      })
 
       lspconfig.lua_ls.setup({
         settings = {
@@ -50,58 +92,3 @@ return {
     end,
   },
 }
-
--- return {
--- 	{
--- 		"williamboman/mason.nvim",
--- 		dependencies = {
--- 			"williamboman/mason-lspconfig.nvim",
--- 			"neovim/nvim-lspconfig",
--- 			"jose-elias-alvarez/null-ls.nvim",
--- 		},
--- 		enabled = true,
---
--- 		config = function()
--- 			require("mason").setup()
---
--- 			local mason_lspconfig = require("mason-lspconfig")
---
--- 			mason_lspconfig.setup({
--- 				ensure_installed = { "gopls", "yamlls", "lua_ls" },
--- 			})
---
--- 			local lspconfig = require("lspconfig")
---
--- 			mason_lspconfig.setup_handlers({
--- 				function(server_name)
--- 					local opts = {}
---
--- 					if server_name == "yamlls" then
--- 						opts = {
--- 							settings = {
--- 								yaml = {
--- 									format = { enable = true },
--- 									schemas = {
--- 										["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
--- 									},
--- 								},
--- 							},
--- 						}
--- 					end
---
--- 					lspconfig[server_name].setup(opts)
--- 				end,
--- 			})
---
--- 			-- ======================================
---
--- 			local null_ls = require("null-ls")
---
--- 			null_ls.setup({
--- 				sources = {
--- 					null_ls.builtins.formatting.stylua,
--- 				},
--- 			})
--- 		end,
--- 	},
--- }
