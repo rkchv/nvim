6deb2d2cd3ab46dd14eed63a100bc59d2574c797
Roman Kochnev <rkochnevv@gmail.com>
2024-12-24 10:15:01 +0300

    add lsp format cmd

--- nvim/.config/nvim/lua/rkchv/config/autocmd.lua
+++ nvim/.config/nvim/lua/rkchv/config/autocmd.lua
@@ -3,7 +3,11 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').goimports()
+    require('go.format').goimports()
   end,
   group = format_sync_grp,
 })
+
+vim.keymap.set("n", "<leader>f", function()
+  require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Format file" })
