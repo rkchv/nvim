ddf8acfee473cda801ddfd617cb2da46adbf7517
Roman Kochnev <rkochnevv@gmail.com>
2025-01-05 10:41:00 +0300

    polish settings

--- nvim/.config/nvim/lua/rkchv/config/set.lua
+++ nvim/.config/nvim/lua/rkchv/config/set.lua
@@ -1,10 +1,3 @@
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "single", -- Options: "none", "single", "double", "rounded", "solid", "shadow"
-  width = 60,        -- Optional: Set the width of the hover window
-  height = 10,       -- Optional: Set the height of the hover window
-})
-
--- vim.opt.silent = true
 vim.opt.guicursor = "n-v-c:block"
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
@@ -12,44 +5,36 @@ vim.opt.number = true
 vim.opt.numberwidth = 3
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
-vim.opt.textwidth = 80
-vim.opt.linebreak = true
 vim.opt.colorcolumn = "80"
-vim.opt.wrap = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-vim.opt.smartindent = true
-vim.opt.breakindent = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
 vim.opt.cmdheight = 0
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.showmode = false
 vim.opt.showmatch = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.isfname:append("@-@")
-vim.opt.nrformats = { "alpha", "octal", "hex" }
-vim.opt.shortmess = "aToIOWAF"
--- vim.opt.showbreak = "↪"
-vim.opt.complete = { ".", "w", "b", "u", "t", "i", "kspell" }
-vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.foldlevel = 10
+vim.opt.foldmethod = "indent"
 vim.opt.fillchars = { foldclose = " ", fold = " ", eob = " " }
-vim.opt.foldlevel = 0
-vim.opt.foldmethod = "marker"
 vim.opt.inccommand = "split"
-vim.opt.wildmode = { "list", "longest" }
 vim.opt.list = false
 vim.opt.listchars = { eol = "↲", tab = "▸ ", trail = "·" }
 vim.opt.virtualedit = { "block" }
 vim.opt.clipboard = "unnamed,unnamedplus"
-vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mouse = "a"
+vim.opt.shortmess = "aToIOWAF"
+
+vim.opt.complete = { ".", "w", "b", "u", "t", "i", "kspell" }
+vim.opt.completeopt = { "menuone" }
+
+vim.opt.textwidth = 80
+vim.opt.linebreak = true
+vim.opt.wrap = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪"
 
 vim.opt.hidden = true
 vim.opt.autoread = true
@@ -64,3 +49,15 @@ vim.opt.timeoutlen = 200
 vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 5
 vim.opt.updatetime = 50
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "single", -- Options: "none", "single", "double", "rounded", "solid", "shadow"
+  width = 60,        -- Optional: Set the width of the hover window
+  height = 10,       -- Optional: Set the height of the hover window
+})
